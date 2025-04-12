610d360edae0959c8304787e926a8038f5213b90
Mahtab <mahtab.jalal.vandi@cern.ch>
2025-04-12 13:06:30 +0330

    Reorganized repo: moved core files to src/, cleaned up old condor jobs

---
+++ condor_jobs/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Detect OS version
+OS=$(uname -r)
+
+if [[ $OS == *"el7"* ]]; then
+    echo "Using CentOS 7"
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_104cuda/x86_64-centos7-gcc11-opt/setup.sh
+elif [[ $OS == *"el8"* ]]; then
+    echo "Using CentOS 8"
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_104cuda/x86_64-centos8-gcc11-opt/setup.sh
+elif [[ $OS == *"el9"* ]]; then
+    echo "Using CentOS 9"
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_104cuda/x86_64-el9-gcc11-opt/setup.sh
+else
+    echo "Unknown OS: $OS"
+fi
+
+# Setup virtual environment
+python -m virtualenv pu-gnn
+source pu-gnn/bin/activate
+
+# Load necessary modules
+module load python/3.8
+module load cuda/11.2
+
+# Copy project from local directory (update this to your current location if needed)
+cp -r ~/Pileup-Distribution-Estimation-GNN-CMS-LHC/PUGNN/PUGNN .
+
+# Run training
+python train_gat.py
